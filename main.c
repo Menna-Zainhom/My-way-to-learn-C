67d121b79e339aa5bbc8d90c24118e3bfbf67b1c
Menna Mohamed <108236971+Menna-Zainhom@users.noreply.github.com>
2022-07-21 12:46:50 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //printf use to print any statement inside it
+    printf("Hello world !\n");
+    printf("I am Menna Mohamed Zainhom\n");
+    return 0;
+}
